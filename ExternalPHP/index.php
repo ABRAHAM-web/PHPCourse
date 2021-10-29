38d99c86f19d27d2e51bfac65aaf202a01bab4db
abraham-git <abrahambrits@protonmail.comls>
2021-10-29 12:17:19 +0200

    Updated course up to External php file

---
+++ ExternalPHP/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>External PHP</title>
+  </head>
+  <body>
+    <h1>External php file </h1>
+    <br>
+    <fieldset>
+      <legend>
+        <form action="" type="POST">
+            <label for="Username : "></label>
+            <input type="text" name="userName"><br>
+            <label for="UserPassword : "></label>
+            <input type="password" name="userPassword"><br>
+            <input type="submit" name="submit">
+        </form>
+      </legend>
+    </fieldset>
+
+  </body>
+</html>
--- post/ExternalPHP.php
+++ post/ExternalPHP.php
@@ -1,24 +1,7 @@
 <?php
-//echo "Hello";
-
- if (isset($_POST["Submit"])) {
-  // echo $_POST["userName"]."<br>";
-  // echo $_POST["password"];
-
-    $username = $_POST["userName"];
-   $password = $_POST["password"];
-
-   echo $username."<br>";
-   echo $password."<br>";
-
-   if ($username == "Gerhard" && $password == "bees") {
-       echo "welcome ".$username."<br>";
-       echo "Cheers<br>";
-     } else {
-      echo "That account doesn't exists";
+    if (isset($_POST['submit'])) {
+      echo $_POST['userName'];
     }
-    // } else {
-    //   echo "Wrong info submitted or no info at all"; }
-}
+
 
  ?>
